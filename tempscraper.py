02e722ab202fb45622210404fad4843a06d944ec
Devansh Agrawal <devagr3924@gmail.com>
2021-05-30 12:25:11 -0500

    setting up

---
+++ tempscraper.py
@@ -0,0 +1,13 @@
+'''
+NOTES:
+The website you are trying to scrape from has been coded in angular, which is why you will not be able to easily scrape with BeautifulSoup.
+Instead, use selenium.
+
+HELPFUL LINKS:
+https://www.youtube.com/watch?v=QoyIWDKrWU0
+for selenium^^
+
+https://www.wunderground.com/history/monthly/us/tx/austin/KAUS/date/2010-10
+page to scrape from^^
+
+'''
